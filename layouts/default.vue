50c4435f6b9e6ee2888c5a20c77069487fea07a8
LeeJinKyu <zlot1112@gmail.com>
2020-10-06 22:08:05 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer v-model="drawer" clipped fixed app temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application</v-list-item-title>
+          <v-list-item-subtitle> subtext</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <!-- v-divider Navi 구분선 -->
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="true" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-menu bottom offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" link>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="true" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false, // 왼쪽네비게이션 오픈여부
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      rightDrawer: false, // 오른쪽 네비게이션 오픈여부
+      alert: false,
+      title: 'Admin Page',
+    }
+  },
+  methods: {
+    alert2() {
+      alert('111')
+    },
+  },
+}
+</script>
